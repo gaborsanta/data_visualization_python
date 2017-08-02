bb54ef588acf952895aee8e9990f777f277d5028
Gabor Santa <Gabor.Santa@gmail.com>
2017-08-02 14:15:06 -0400

    add dice files

--- scatter_squares.py
+++ scatter_squares.py
@@ -23,4 +23,5 @@ plt.axis([0, 1100, 0, 1100000])
 # Set size of tick labels.
 plt.tick_params(axis='both', which='major', labelsize=14)
 
-plt.show()
+#plt.show()
+plt.savefig('squares_plot.png', bbox_inches='tight')
